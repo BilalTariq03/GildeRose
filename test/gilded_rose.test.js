0e7f7b81f4f44be2a00a3714f2ea1e8149729fcc
Bilal Tariq <billal.tariq03@gmail.com>
2026-09-18 00:38:32 +0500

    Added tests for Sulfuras

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -76,4 +76,25 @@ describe("Gilded Rose - Aged Brie", function() {
 });
 
 
+describe("Gilded Rose - Sulfuras", function() {
+  it("Nothing changes for sulfurus", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 4, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(4);
+    expect(items[0].quality).toBe(80);
+  });
 
+  it("Nothing changes for sulfurus", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("Nothing changes for sulfurus", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -5, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-5);
+    expect(items[0].quality).toBe(80);
+  });
+});
